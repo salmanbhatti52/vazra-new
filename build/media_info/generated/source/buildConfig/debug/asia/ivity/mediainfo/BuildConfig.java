1613a67e8a67591c9795b2d63189cf4924eb1812
TehreemHashmi09 <91934907+TehreemHashmi09@users.noreply.github.com>
2023-09-14 17:02:56 -0700

    update

---
+++ build/media_info/generated/source/buildConfig/debug/asia/ivity/mediainfo/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package asia.ivity.mediainfo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "asia.ivity.mediainfo";
+  public static final String BUILD_TYPE = "debug";
+}
